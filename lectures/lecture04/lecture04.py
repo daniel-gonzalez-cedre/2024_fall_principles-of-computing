154b437a96b03a67063cd46eb05d5d196599a844
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2024-09-11 22:37:04 -0400

    [add] in-class code from lecture 04
    Changes to be committed:
            modified:   lecture04/lecture04.py

--- lectures/lecture04/lecture04.py
+++ lectures/lecture04/lecture04.py
@@ -1,45 +1,50 @@
+# get the user's first name
 def get_first_name():
-  full_name = input("Enter your full legal name here: ")
-  modified_name = full_name + " "
+  full_name = input("Enter your full legal name here: ")  # prompt the user for their full name
 
-  index_of_space = modified_name.index(" ")
+  modified_name = full_name + " "  # to avoid edge-cases that would cause the next line to crash
 
-  first_name = full_name[0:index_of_space]
-  return first_name
+  index_of_space = modified_name.index(" ")  # this line throw an error if `modified_name` does not have any spaces
+
+  first_name = full_name[0:index_of_space]  # slice the full name up to the index of the first space
+  return first_name  # output the first name
 
 
+# get a username (CASE-INSENSITIVE) from the user
 def get_username(x):
-  print("Hello, " + x + ".")
-  y = input("Please enter your desired username: ")
-  z = y.lower()
-  return z
+  print("Hello, " + x + ".")  # greet the user
+  y = input("Please enter your desired username: ")  # prompt the user for their username
+  z = y.lower()  # force the provided username to lowercase
+  return z  # output the lower-cased username
 
 
+# obtain a password (CASE-SENSITIVE) from the user
+# make user confirm their password by entering it twice
+# ensure the password is at least 8 characters long
 def get_password():
-  y1 = input("Please enter your password: ")
-  y2 = input("Confirm your password: ")
+  y1 = input("Please enter your password: ")  # prompt the user for their password
+  y2 = input("Confirm your password: ")  # prompt the user to confirm their password
 
-  if y1 == y2:
-    if len(y1) < 8:
-      1/0
+  if y1 == y2:  # check that y1 is EQUAL to y2
+    if len(y1) <= 7:  # check that y1 is long enough
+      1/0  # CRASH: password is too short
     else:
-      return y1
+      return y1  # return the password
   else:
-    1/0
+    1/0  # CRASH: passwords do not match
 
 
+# simulate the login process
 def test_login(x, y):
   print("Simulating login procedure.")
   uname = input("Username: ")
   passwd = input("Password: ")
 
-  if uname.lower() == x:
-    if passwd == y:
+  if uname.lower() == x:  # enure usernames match
+    if passwd == y:  # ensure passwords match
       print("Access granted.")
-      return True
 
   print("Access denied.")
-  return False
 
 
 def login_setup():
